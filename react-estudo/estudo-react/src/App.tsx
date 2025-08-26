395de59a00b056bb5fce5d0cfbb6cd3261594468
VitoriaMaglio <vitoriamaglio@gmail.com>
2025-08-26 08:28:14 -0300

    Entendendo componentes reaproveitáves.

--- react-estudo/estudo-react/src/App.tsx
+++ react-estudo/estudo-react/src/App.tsx
@@ -1,7 +1,14 @@
+import { Outlet } from "react-router-dom";
+import Rodape from "./components/Rodape";
+import Menu from "./components/Menu";
+import './App.css'
+
 export default function App() {
   return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
+    <>
+      <Menu/>
+      <Outlet/>
+      <Rodape/>
+    </>
   )
 }
